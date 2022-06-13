90439b6ae60eda78a652c2399d3b5d8bb2b0ac86
Italo Siqueira Lima <italosiqueira@gmail.com>
2022-06-13 02:37:22 -0300

    feat(Dia 2): exercício do Dia 02

--- app/src/main/java/br/com/alura/devhub/MainActivity.kt
+++ app/src/main/java/br/com/alura/devhub/MainActivity.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.alura.devhub.ui.theme.DevHubTheme
@@ -57,24 +63,58 @@ fun DisplayTextField(field: OutputTextField) {
 @Composable
 fun DefaultPreview() {
     DevHubTheme {
-//        DisplayTextField(OutputTextField("Nome", "Italo Siqueira Lima"))
-        Column {
-            Image(
-                painter = painterResource(id = R.drawable.profile_picture),
-                contentDescription = "Profile Picture",
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val  boxHeight = remember {
+                150.dp
+            }
+
+            val imageHeight = remember {
+                boxHeight
+            }
+
+            Box(
                 modifier = Modifier
-                    // Set image size to 40 dp
-                    .size(80.dp)
-                    // Clip image to be shaped as a circle
-                    .clip(CircleShape)
-            )
+                    .fillMaxWidth()
+                    .background(
+                        color = Color(0xFF2d333b),
+                        shape = RoundedCornerShape(
+                            bottomEnd = 16.dp,
+                            bottomStart = 16.dp,
+                        )
+                    )
+                    .height(boxHeight)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile_picture),
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier
+                        .offset(y = imageHeight / 2)
+                        // Set image size to 80 dp
+                        .size(imageHeight)
+                        .align(Alignment.BottomCenter)
+                        // Clip image to be shaped as a circle
+                        .clip(CircleShape)
+                )
+            }
 
             // Add a horizontal space between the image and the column
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(imageHeight / 2))
 
-            Text(text = "Italo Siqueira Lima")
-            Text(text = "italosiqueira")
-            Text(text = "Desenvolvedor e Analista de Sistemas")
+            Text(
+                text = "Italo Siqueira Lima",
+                style = MaterialTheme.typography.h6
+            )
+            Text(
+                text = "@italosiqueira",
+                style = MaterialTheme.typography.caption,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "Desenvolvedor e Analista de Sistemas", style
+                = MaterialTheme.typography.body2
+            )
         }
     }
 }
