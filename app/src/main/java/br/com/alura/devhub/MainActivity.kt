4837884a5a35cf262b8343f0b47e5b7bd53a1b17
Italo Siqueira Lima <italosiqueira@gmail.com>
2022-06-16 02:56:53 -0300

    feat(Dia 3): implementa consulta dinâmica de imagem

--- app/src/main/java/br/com/alura/devhub/MainActivity.kt
+++ app/src/main/java/br/com/alura/devhub/MainActivity.kt
@@ -17,11 +17,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.alura.devhub.ui.theme.DevHubTheme
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -86,9 +90,14 @@ fun DefaultPreview() {
                     )
                     .height(boxHeight)
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.profile_picture),
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("https://avatars.githubusercontent.com/u/1196586?v=4")
+                        .crossfade(true)
+                        .build(),
+                    placeholder = painterResource(R.drawable.profile_picture_placeholder),
                     contentDescription = "Profile Picture",
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier
                         .offset(y = imageHeight / 2)
                         // Set image size to 80 dp
